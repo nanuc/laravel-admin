b222c855a37830183a8ae9f107700943201a6f65
Sebastian Schöps <sebastian.schoeps@nanuc.io>
2022-01-13 18:21:02 +0100

    Impersonation style

--- resources/views/components/impersonation.blade.php
+++ resources/views/components/impersonation.blade.php
@@ -1,16 +1,16 @@
 @impersonating
-    <div class="relative bg-indigo-600">
-        <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
-            <div class="pr-16 sm:text-center sm:px-16">
-                <p class="font-medium text-white">
-                    <span class="hidden md:inline">
+<div class="relative bg-red-800">
+    <div class="max-w-7xl mx-auto py-1 px-1 sm:px-6 lg:px-8">
+        <div class="pr-16 sm:text-center sm:px-16">
+            <p class="text-sm text-white">
+                    <span class="md:inline">
                         {{ auth()->user()->name ?? auth()->user()->email }} is being impersonated.
                     </span>
-                    <span class="block sm:ml-2 sm:inline-block">
+                <span class="block sm:ml-2 sm:inline-block">
                         <a href="{{ route('impersonate.leave') }}" class="text-white font-bold underline"> Exit impersonation <span aria-hidden="true">&rarr;</span></a>
                     </span>
-                </p>
-            </div>
+            </p>
         </div>
     </div>
+</div>
 @endImpersonating
